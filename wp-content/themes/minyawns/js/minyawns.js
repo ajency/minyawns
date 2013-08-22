63c472bb2579fe374bf5059a85cd16f06cc89b64
diasansley <ansley@ajency.in>
2013-08-22 10:54:21 +0530

    wages field numeric

--- wp-content/themes/minyawns/js/minyawns.js
+++ wp-content/themes/minyawns/js/minyawns.js
@@ -8,7 +8,18 @@ jQuery(document).ready(function($) {
     /********************************** PROFILE JS CODE *************************************/
 
 
-
+$("#job_wages").keydown(function(event) {
+    	// Allow only backspace and delete
+    	if ( event.keyCode == 46 || event.keyCode == 8 ) {
+    		// let it happen, don't do anything
+    	}
+    	else {
+    		// Ensure that it is a number and stop the keypress
+    		if (event.keyCode < 48 || event.keyCode > 57 ) {
+    			event.preventDefault();	
+    		}	
+    	}
+    });
 
     if (jQuery('#user-popdown').length > 0)
     {
