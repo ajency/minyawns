b5b2f972129c83c5b0781d68af59a2347d73c8ab
Amit Adav <amit@ajency.in>
2013-09-02 13:39:20 +0530

    page added

--- wp-content/themes/minyawns/profile.php
+++ wp-content/themes/minyawns/profile.php
@@ -19,7 +19,7 @@ get_header();  ?>
     <div class="accordion-heading">
     <a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion2" href="#collapse<%= post_id %>">
     <div class="span12 data-title available">
-    <div class="job-logo header-sub"> <img src="<%= job_author_logo %>"/></div>
+    <div class="job-logo header-sub"> <div class="logo"><img src="<%= job_author_logo %>"/></div></div>
     <div class="job-date header-sub">
     <span class="service-total-demand" data-count="0"><%= job_start_day %></span>
     <div>
