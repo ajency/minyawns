91d4881cd73de546c9efa53a0b696e96a3732e45
Parag Redkar <parag@ajency.in>
2013-09-03 13:19:09 +0530

    cancel payment

--- wp-content/themes/minyawns/cancel-payemnt.php
+++ wp-content/themes/minyawns/cancel-payemnt.php
@@ -17,6 +17,7 @@ if(isset($_POST['mntx']))
 	
 	global $wpdb;
 	$paypal_tx  = $wpdb->get_results("SELECT meta_value as paypal_payment FROM {$wpdb->prefix}postmeta WHERE meta_key ='paypal_payment' and post_id ='".$jobid."' AND meta_value like '%".$minyawns_tx."%'  ");
+	echo "SELECT meta_value as paypal_payment FROM {$wpdb->prefix}postmeta WHERE meta_key ='paypal_payment' and post_id ='".$jobid."' AND meta_value like '%".$minyawns_tx."%'  ";
 	
 	foreach($paypal_tx as $res)
 	{
@@ -26,6 +27,8 @@ if(isset($_POST['mntx']))
 	$new_paypal_payment = array();
 	foreach($paypal_payment as $key_pp => $payment_tx)
 	{
+		echo $key_pp." => ".$payment_tx;
+		
 		switch($key_pp)
 		{
 			case 'minyawn_txn_id':
@@ -49,7 +52,7 @@ if(isset($_POST['mntx']))
 	//update postmeta for the job with transaction id
 	$new_updated_paypal_payment =   serialize($new_paypal_payment);
 	$wpdb->get_results("update {$wpdb->prefix}postmeta  set meta_value = '".$new_updated_paypal_payment."' WHERE post_id = ".$jobid." and meta_key ='paypal_payment'  AND    meta_value like '%".$minyawns_tx_id."%'");
-	
+	echo "update {$wpdb->prefix}postmeta  set meta_value = '".$new_updated_paypal_payment."' WHERE post_id = ".$jobid." and meta_key ='paypal_payment'  AND    meta_value like '%".$minyawns_tx_id."%'";
 	
 	
 	
