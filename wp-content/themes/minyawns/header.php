627ce56d070ab17df75dba885ee111902c51b5f6
Amit Adav <amit@ajency.in>
2013-09-04 17:43:08 +0530

    google code added

--- wp-content/themes/minyawns/header.php
+++ wp-content/themes/minyawns/header.php
@@ -34,6 +34,16 @@
 	
 	<!-- wordpress head functions -->
 	<?php wp_head(); ?>
+	<script>
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+ga('create', 'UA-43096826-1', 'minyawns.com');
+ga('send', 'pageview');
+
+</script>
 	</head>
         <script>
             var siteurl='<?php echo site_url(); ?>';
