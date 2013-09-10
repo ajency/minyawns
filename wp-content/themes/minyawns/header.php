c76f9e84797794b064d38f07411dcb4402f323a4
diasansley <ansley@ajency.in>
2013-09-10 12:31:46 +0530

    ga code changed

--- wp-content/themes/minyawns/header.php
+++ wp-content/themes/minyawns/header.php
@@ -40,7 +40,7 @@
   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
   })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-  ga('create', 'UA-43815369-1', 'minyawns.com');
+  ga('create', 'UA-43096826-1', 'minyawns.com');
   ga('send', 'pageview');
 
 </script>
