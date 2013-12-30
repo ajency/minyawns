0a40529cb9df659b56b0245ce7643018bdb0605a
Parag Redkar <parag@ajency.in>
2013-12-30 12:38:56 +0530

    big screen issue fix for firefox

--- wp-content/themes/minyawns/footer.php
+++ wp-content/themes/minyawns/footer.php
@@ -1,4 +1,4 @@
-
+<a href="#" class="scrollup">Scroll</a>
 
 <script type="text/javascript">
 var ajaxurl = "<?php echo admin_url( 'admin-ajax.php' ); ?>"
@@ -23,6 +23,19 @@ jQuery(document).ready(function($) {
  jQuery('.carousel').carousel({
 	interval: 2000
 		})
+		
+		  $(window).scroll(function(){
+            if ($(this).scrollTop() > 100) {
+                $('.scrollup').fadeIn();
+            } else {
+                $('.scrollup').fadeOut();
+            }
+        }); 
+ 
+        $('.scrollup').click(function(){
+            $("html, body").animate({ scrollTop: 0 }, 600);
+            return false;
+        });
 		  });
 </script>
 <?php } ?>
