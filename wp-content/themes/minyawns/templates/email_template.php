86f921cae35e9fac8ca11f1bb1bb836175aba043
diasansley <ansley@ajency.in>
2014-01-03 12:45:02 +0530

    email format

--- wp-content/themes/minyawns/templates/email_template.php
+++ wp-content/themes/minyawns/templates/email_template.php
@@ -262,8 +262,8 @@ function get_email_msg($type, $data, $emailid) {
              return $email_msg;
              
         case 'invite_minion':$email_msg="Hi,<br/><br/> Well done! Your profile has caught the attention of an employer. You are one of the handpicked minions invited to apply for the job.<br/><br/>
-<b>Job details</b><br/>Job description:".$data['decription']."
-Wages: ".$data['wages']."
+<b>Job details</b><br/>Job description:".$data['decription']."<br/>
+Wages: ".$data['wages']."<br/>
 Report for duty at ".$data['time']." on ".$data['date']."<br/>
 You are at an advantage over other minions as you are invited by the employer after going over your profile and checking if you meet the criteria for the job.
 Sounds good? Go ahead.<br/> 
