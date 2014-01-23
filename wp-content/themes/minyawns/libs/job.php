506b3cfac6292670242ea0ff0d332108777fd159
diasansley <ansley@ajency.in>
2014-01-23 11:07:17 +0530

    job mail filter

--- wp-content/themes/minyawns/libs/job.php
+++ wp-content/themes/minyawns/libs/job.php
@@ -822,10 +822,10 @@ $app->post('/inviteminions', function() use($app) {
             $mail = email_template($emailid, $data_mail, 'invite_minion');
             $headers = 'From: support@minyawns.com' . "\r\n";
             $headers .= "MIME-Version: 1.0\n" .
-                    "From:From: support@minyawns.com\n" .
+                    "From: Minyawns support@minyawns.com\n" .
                     "Content-Type: text/html; charset=\"" . "\"\n";
-           print_r($emailid);
-            wp_mail('ansley@ajency.in', $mail['subject'], $mail['hhtml'] . $mail['message'] . $mail['fhtml'], $headers);
+          
+            wp_mail($emailid, $mail['subject'], $mail['hhtml'] . $mail['message'] . $mail['fhtml'], $headers);
 
             $requestBody = $app->request()->getBody();  // <- getBody() of http reques
             $json_a = json_decode($requestBody, true);
