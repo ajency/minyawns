1640a7ac2c687c46f5d4f3e106c4e33215ad333a
parag redkar <parag@ajency.in>
2014-05-15 18:04:02 +0530

    revert

--- wp-content/themes/minyawns/templates/_jobs.php
+++ wp-content/themes/minyawns/templates/_jobs.php
@@ -280,7 +280,7 @@ And you don't need a PayPal account to pay us.<br> Any credit or debit card will
     
 
 							 <div class="row-fluid mobile-hide" >
-							  <div class="<% if(currentpage_user_role=="employer"){%>span9<%} else{ %>span6<%} %> ">
+							  <div class="<% /*if(currentpage_user_role=="employer"){ span9 } else{ */ %>span6<% /* } */  %> ">
 							       <div class="row-fluid " data-toggle="collapse-next" data-parent="#accordion24">
                                       <div class="span1">
 									  <div class="job-date">
@@ -304,7 +304,7 @@ And you don't need a PayPal account to pay us.<br> Any credit or debit card will
                                        </div>
                                     </div>
 							  </div>
-							  <div class="<% if(currentpage_user_role=="employer"){%>span3<%} else{ %>span4<% } %> status">
+							  <div class="<% /* if(currentpage_user_role=="employer"){ span3< else{ */ %>span4<% /* } */ %> status">
 							    <div class="st-moile-span1">
 
                                           <div class="st-wages"> wages <b>$<%= result.job_wages %></b></div>
@@ -313,7 +313,7 @@ And you don't need a PayPal account to pay us.<br> Any credit or debit card will
       </a>
                                        </div>
 							  </div>
-				<% if(currentpage_user_role!="employer"){%>
+				<% /*if(currentpage_user_role!="employer"){ */ %>
                  <div class="span2">
 
                   		<div class="st-moile-span1">
@@ -321,7 +321,7 @@ And you don't need a PayPal account to pay us.<br> Any credit or debit card will
                          </div>
 
                   </div> 
-				<% }%>
+				<% /*} */ %>
 
 			   </div> 
                </div>
