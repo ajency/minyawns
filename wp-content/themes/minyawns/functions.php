8cdea0f8f452c1438f4f3d348305f72ced294091
diana29 <diana@ajency.in>
2014-10-04 13:52:39 +0530

    user info function moved to  functions.php

--- wp-content/themes/minyawns/functions.php
+++ wp-content/themes/minyawns/functions.php
@@ -27,8 +27,58 @@ require_once 'cron_functions.php';
 
 require_once 'templates/email_template.php';
 
-//load the functions related to users
-require_once '/functions/users.php';
+function get_miny_current_user(){
+
+
+    global $user_ID,  $wp_roles ;
+
+    $miny_current_user = array();
+    $user_info = wp_get_current_user(); 
+
+ 
+      if($user_info->data->user_id!=NULL){
+
+        
+        $miny_current_user['id'] = $user_ID;
+
+        $miny_current_user['user_login'] = $user_info->data->user_login;
+
+        $miny_current_user['user_email'] = $user_info->data->user_email;
+
+        $miny_current_user['display_name'] = $user_info->data->display_name; 
+
+        $miny_current_user['role'] =  key($user_info->caps) ;
+
+        $miny_current_user['display_role'] = $wp_roles->role_names[key($user_info->caps)] ;
+
+        $all_caps = array();
+
+        foreach($user_info->allcaps as $key=>$capability){
+
+            $all_caps[] = $key;
+
+        }
+         $miny_current_user['all_caps'] =$all_caps ;
+    }else{
+
+         $miny_current_user['id'] = 0;
+
+         $miny_current_user['user_login'] = '';
+
+        $miny_current_user['user_email'] = '';
+
+        $miny_current_user['display_name'] = ''; 
+
+        $miny_current_user['role'] =  '' ;
+
+        $miny_current_user['display_role'] = '' ;
+
+        $miny_current_user['all_caps'] = array() ;
+
+    }
+
+    return  $miny_current_user ;
+}
 
 //remove admin bar from front end
 show_admin_bar(false);
