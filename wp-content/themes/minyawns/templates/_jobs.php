72e396cb00a57676420f84cd30db2e392ae70b0b
nevesseq <neves@ajency.in>
2014-11-01 10:45:43 +0530

    toggle-button and raatings styled on jobs-profile page

--- wp-content/themes/minyawns/templates/_jobs.php
+++ wp-content/themes/minyawns/templates/_jobs.php
@@ -1,4 +1,4 @@
-<script type="text/template" id="jobs-table"> 
+	<script type="text/template" id="jobs-table"> 
       <li class="_li <% if(result.todays_date_time > result.job_end_date_time_check) {%>job-closed<%}else{%>job-open<%}%> panel">
       
 							 <div class="row-fluid mobile-hide" >
@@ -247,158 +247,7 @@ $current_user_role =  trim($user_role);
  <%}%>
 <div class="span12">
 	<div class="row-fluid">
-		<div class="span6">
-			<!--comment starts-->
-  		<div class="msg-cover">
-    		<p class="msg left">Enter your Message here</p>
-    		<textarea class="full m-b-10" rows="3" ></textarea>
-    		<div class="right m-b-10">
-      		<input type="submit" class="btn green-btn" value="Post Message">
-      		<input type="submit" class="btn" value="Cancel">
-    		</div>
-		
-
-  		<div class="avatar-container">
-  			<div class="avatar-box">
-  				<div class="avatar left" href="#">
-  					<img src="http://bignono2.karmacracy.com/img/privateNew/icons/cookies.png" class="avatar-img">
-  				</div>
-  				<div class="avatar-content">
-  					<h5 class="avatar-heading left">Lorem ipsum dolor sit amet </h5>
-					<h5 class="avatar-heading left full-width"><small class="member-status"> Minyawn</small><small class="job-status"> Applied</small></h5>
-  					<p class="comment m-tb-5">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo.</p>
-
-  					<div class="comment-info m-b-10">
-  						<span class="comment-date left">
-  							14 July 2016
-  						</span>
-  						<span class="left">&nbsp;|&nbsp;</span>
-  						<span class="comment-time left">
-  						9:30 am
-  						</span>
-
-  						<span class="right rep-del">
-  							<a href="#" class="reply">
-  								<span class="glyphicon glyphicon-share-alt"></span>
-  							</a>&nbsp;
-  							<a href="#" class="delete">
-  								<span class="glyphicon glyphicon-trash" onclick="$('.alert-msg').show()"></span>
-  							</a>
-
-  						</span>
-  					</div>
-
-				
-    				</div>
-    				<div class="alert-msg">
-    					<div class="icon-close right">
-    						<a href="#" onclick="$('.alert-msg').hide()"><span class="glyphicon glyphicon-remove"></span></a>
-    					</div>
-    					Successfully deleted the message
-    				</div>
-    			</div>
-
-			    <div class="avatar-box">
-    				<div class="avatar left" href="#">
-    					<img src="http://bignono2.karmacracy.com/img/privateNew/icons/cookies.png" class="avatar-img">
-    				</div>
-				
-    				<div class="avatar-content">
-    					<h5 class="avatar-heading left">Dorlem sipsum color sit amet</h5>
-    					<p class="comment m-tb-5">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem.</p>
-    					<div class="comment-info m-b-10">
-      					<span class="comment-date left">
-      					15 July 2016
-      					</span>
-      					<span class="left">&nbsp;|&nbsp;</span>
-      					<span class="comment-time left">
-      					10:45 am
-      					</span>
-
-    					  <span class="right rep-del">
-    							<a href="#" class="reply" onclick="$('.reply-txt').show();$('.rep-del').hide();">
-    								<span class="glyphicon glyphicon-share-alt"></span>
-    							</a>&nbsp;
-    							<a href="#" class="delete">
-    								<span class="glyphicon glyphicon-trash"></span>
-    							</a>
-    						</span>
-
-						    <div class="reply-txt">
-          				<p class="reply-msg left">Enter your Reply here</p>
-          				<textarea class="full m-tb-10" rows="3"></textarea>
-          				<div class="right m-b-10">
-          					<input type="submit" class="btn green-btn" value="Post Reply" onclick="$('.avatar-box-1').show();$('.reply-txt').hide();">
-          					<input type="submit" class="btn" value="Cancel" onclick="$('.reply-txt').hide();$('.rep-del').show();">
-          				</div>
-				        </div>
-				  	  </div>
-				    </div>
-
-            <!--child-1-->				
-            <div class="avatar-box-1">
-      				<div class="avatar left" href="#">
-      					<img src="http://bignono2.karmacracy.com/img/privateNew/icons/cookies.png" class="avatar-img">
-      				</div>
-      				<div class="avatar-content">
-      					<h5 class="avatar-heading left">Dorlem sipsum color sit amet</h5>
-					      <p class="comment m-tb-5">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem.</p>
-        					<div class="comment-info m-b-10">
-          					<span class="comment-date left">
-          					15 July 2016
-          					</span>
-          					<span class="left">&nbsp;|&nbsp;</span>
-          					<span class="comment-time left">
-          					 10:45 am
-          					</span>
-          					<span class="right rep-del">
-          							<a href="#" class="reply" onclick="$('.reply-txt').show();$('.rep-del').hide();">
-          								<span class="glyphicon glyphicon-share-alt"></span>
-          							</a>&nbsp;
-          							<a href="#" class="delete">
-          								<span class="glyphicon glyphicon-trash"></span>
-          							</a>
-          						</span>
-          					</div>
-          				</div>
-          			</div>
-          			<!--child-1 ends-->
-
-                <!--child-2-->				
-                <div class="avatar-box-2">
-          				<div class="avatar left" href="#">
-          					<img src="http://bignono2.karmacracy.com/img/privateNew/icons/cookies.png" class="avatar-img">
-          				</div>
-          				<div class="avatar-content">
-          					<h5 class="avatar-heading left">Dorlem sipsum color sit amet</h5>
-					          <p class="comment m-tb-5">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem.</p>
-          					<div class="comment-info m-b-10">
-            					<span class="comment-date left">
-            					15 July 2016
-            					</span>
-            					<span class="left">&nbsp;|&nbsp;</span>
-            					<span class="comment-time left">
-            					10:45 am
-            					</span>
-
-            					<span class="right rep-del">
-            							<a href="#" class="reply" onclick="$('.reply-txt').show();$('.rep-del').hide();">
-            								<span class="glyphicon glyphicon-share-alt"></span>
-            							</a>&nbsp;
-            							<a href="#" class="delete">
-            								<span class="glyphicon glyphicon-trash"></span>
-            							</a>
-						            </span>
-					            </div>
-				            </div>
-            			</div>
-            			<!--child-2 ends-->
-                </div>
-		          </div>
-            </div>
-			      <!--comment-ends-->
-      		</div>
-      		<div class="span6">
+	<div class="span7">
 			
 			<div class=" mobile-alert-box-hidden">
               <!--<div class="alert alert-success alert-sidebar author-data">
@@ -432,15 +281,195 @@ $current_user_role =  trim($user_role);
               <%}%>      
             </div>
 			
+			<div class="row-fluid">
+			<div class="span12">
+			<h3 class="pull-left custom-h3">Applicants</h3>
+			</div>
+			</div>
             <ul class="thumbnails">
               <span class='load_ajaxsingle_job_minions' style="display:none"></span>
-			  <a href="#" class="pull-right">View All &raquo;</a>
+			  <div class="row-fluid">
+			  <div class="span12">
+			  <a href="#" class="pull-right view-all-app">View All Applicants &raquo;</a>
+			  </div>
+			  </div>
             </ul>
             </br></br><span id="div_confirmhire"></span>
 
             
 
 	        </div>
+		<div class="span5">
+			<!--comment starts-->
+  		<div class="msg-cover">
+    		<p class="msg left">Enter your Message here</p>
+    		<textarea class="full m-b-10" rows="3" ></textarea>
+    		<div class="right m-b-10">
+      		<input type="submit" class="btn green-btn" value="Post Message">
+      		<input type="submit" class="btn" value="Cancel">
+    		</div>
+		
+
+  		<div class="avatar-container">
+  			<div class="avatar-box">
+  				<div class="avatar left" href="#">
+  					<img src="http://images.veerle.duoh.com/uploads/inspiration-images/my-name-is-buck.jpg" class="avatar-img">
+  				</div>
+  				<div class="avatar-content">
+  					<h5 class="avatar-heading left">Alice Benz</h5>
+					<div class="comment-info m-b-10">
+  						<span class="comment-date right">
+  							14 July 2016
+  						</span>
+  						<span class="right">&nbsp;,&nbsp;</span>
+  						<span class="comment-time right">
+  						9:30 am
+  						</span>
+					</div>
+					<h5 class="avatar-heading left full-width"><small class="member-status"> Minyawn</small><small class="job-status"> Applied</small></h5>
+  					<p class="comment m-tb-5">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo.</p>
+
+  					<div class="comment-info m-b-10">
+  						
+
+  						<span class="left rep-del">
+						<a href="#" class="comments">
+  								comments(3)
+  							</a>|
+  							<a href="#" class="reply">
+  								reply
+  							</a>|
+  							<a href="#" class="delete">
+  								delete
+  							</a>
+
+  						</span>
+  					</div>
+
+				
+    				</div>
+    				<div class="alert-msg">
+    					<div class="icon-close right">
+    						<a href="#" onclick="$('.alert-msg').hide()"><span class="glyphicon glyphicon-remove"></span></a>
+    					</div>
+    					Successfully deleted the message
+    				</div>
+    			</div>
+
+			    <div class="avatar-box">
+    				<div class="avatar left" href="#">
+    					<img src="http://images.veerle.duoh.com/uploads/inspiration-images/my-name-is-buck.jpg" class="avatar-img">
+    				</div>
+				
+    				<div class="avatar-content">
+    					<h5 class="avatar-heading left">John Smith</h5>
+    					<p class="comment m-tb-5">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem.</p>
+    					<!--<div class="comment-info m-b-10">
+      					<span class="comment-date left">
+      					15 July 2016
+      					</span>
+      					<span class="left">&nbsp;|&nbsp;</span>
+      					<span class="comment-time left">
+      					10:45 am
+      					</span>
+
+    					  <span class="right rep-del">
+    							<a href="#" class="reply" onclick="$('.reply-txt').show();$('.rep-del').hide();">
+    								<span class="glyphicon glyphicon-share-alt"></span>
+    							</a>&nbsp;
+    							<a href="#" class="delete">
+    								<span class="glyphicon glyphicon-trash"></span>
+    							</a>
+    						</span>
+
+						    <div class="reply-txt">
+          				<p class="reply-msg left">Enter your Reply here</p>
+          				<textarea class="full m-tb-10" rows="3"></textarea>
+          				<div class="right m-b-10">
+          					<input type="submit" class="btn green-btn" value="Post Reply" onclick="$('.avatar-box-1').show();$('.reply-txt').hide();">
+          					<input type="submit" class="btn" value="Cancel" onclick="$('.reply-txt').hide();$('.rep-del').show();">
+          				</div>
+				        </div>
+				  	  </div>-->
+					  <div class="comment-info m-b-10" id="mf276">
+  						
+
+  						<span class="left rep-del" id="mf277">
+						<a href="#" class="comments" id="mfa22">
+  								comments(3)
+  							</a>|
+  							<a href="#" class="reply" id="mfa23">
+  								reply
+  							</a>|
+  							<a href="#" class="delete" id="mfa24">
+  								delete
+  							</a>
+
+  						</span>
+  					</div>
+				    </div>
+
+            <!--child-1-->				
+            <div class="avatar-box-1">
+      				<div class="avatar left" href="#">
+      					<img src="http://images.veerle.duoh.com/uploads/inspiration-images/my-name-is-buck.jpg" class="avatar-img">
+      				</div>
+      				<div class="avatar-content">
+      					<h5 class="avatar-heading left">Dorlem sipsum color sit amet</h5>
+					      <p class="comment m-tb-5">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem.</p>
+        					<div class="comment-info m-b-10" id="mf199">
+  						
+
+  						<span class="left rep-del" id="mf200">
+						<a href="#" class="comments" id="mfa14">
+  								comments(3)
+  							</a>|
+  							<a href="#" class="reply" id="mfa15">
+  								reply
+  							</a>|
+  							<a href="#" class="delete" id="mfa16">
+  								delete
+  							</a>
+
+  						</span>
+  					</div>
+          				</div>
+          			</div>
+          			<!--child-1 ends-->
+
+                <!--child-2-->				
+                <div class="avatar-box-2">
+          				<div class="avatar left" href="#">
+          					<img src="http://images.veerle.duoh.com/uploads/inspiration-images/my-name-is-buck.jpg" class="avatar-img">
+          				</div>
+          				<div class="avatar-content">
+          					<h5 class="avatar-heading left">William Spoke</h5>
+					          <p class="comment m-tb-5">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem.</p>
+          					<div class="comment-info m-b-10" id="mf199">
+  						
+
+  						<span class="left rep-del" id="mf200">
+						<a href="#" class="comments" id="mfa14">
+  								comments(0)
+  							</a>|
+  							<a href="#" class="reply" id="mfa15">
+  								reply
+  							</a>|
+  							<a href="#" class="delete" id="mfa16">
+  								delete
+  							</a>
+
+  						</span>
+  					</div>
+				            </div>
+            			</div>
+            			<!--child-2 ends-->
+                </div>
+		          </div>
+            </div>
+			      <!--comment-ends-->
+      		</div>
+      		
 	      </div>
       </div>
 
@@ -602,8 +631,8 @@ $current_user_role =  trim($user_role);
 
 <script type="text/template" id="minion-cards">
 
-	<div class="row-fluid">
-    <li class="span3 thumbspan" id="<%= result.user_id %>" >
+	<!--<div class="row-fluid">-->
+    <li class="thumbspan" id="<%= result.user_id %>" >
 	
     <div class="thumbnail select-button-<%= result.user_id %>" id="thumbnail-<%= result.user_id %>">
     <div class="layer">
@@ -681,7 +710,7 @@ $current_user_role =  trim($user_role);
     <div class="caption m4" >
     <div>
 	<div class="row-fluid">
-	<div class="span3">
+	<div class="span4">
     <div class="minyawns-img" >
         <% if(result.is_hired === true){%>
           <div class="minaywns-sel">
@@ -700,7 +729,7 @@ $current_user_role =  trim($user_role);
     </a>
     </div>
 	</div>
-	<div class="span9">
+	<div class="span8">
 	<div class="row-fluid">
 	<div class="span8">
     <h4 class="align-left"><a href='<?php echo site_url(); ?>/profile/<%= result.user_id %>' target="_blank"> <%= result.name %></a></h4>
@@ -756,13 +785,22 @@ $current_user_role =  trim($user_role);
 	</div>	
 			</div>
 			</div>
-			<div>
-        <div class="pull-right">
+</div>
+			<div class="">
           <%= select_button %>
+		  
+		  
         </div>
+		
+		<div class="dwn-btn review_popover">
+
+   <%= ratings_button %>
+<!--   <%  if(result.comment !== 0){ %><div  class="comment-box"> <i class="icon-thumbs-up weldone"></i> <%= result.comment %></div><% }else{%><div  class="comment-box"> <i class="icon-thumbs-down terrible"></i> <%= result.comment %></div><%}%>-->
+   </div>
+		  
+	
         <div class="clearfix"></div>
-			</div>
-			</div>
+			
     </div>
 
 
@@ -791,11 +829,11 @@ $current_user_role =  trim($user_role);
     <%  if(result.comment !== 0){ %>   <div  class="comment-box"> <i class="icon-thumbs-up weldone"></i><%= result.comment %></div><% } %>
 
     </div>-->
-    <div class="dwn-btn review_popover">
+    <!--<div class="dwn-btn review_popover">-->
 
-   <%= ratings_button %>
+   <!--<%= ratings_button %>-->
 <!--   <%  if(result.comment !== 0){ %><div  class="comment-box"> <i class="icon-thumbs-up weldone"></i> <%= result.comment %></div><% }else{%><div  class="comment-box"> <i class="icon-thumbs-down terrible"></i> <%= result.comment %></div><%}%>-->
-   </div>
+   <!--</div>-->
 
     
     <!--<div class="onoffswitch">
@@ -806,7 +844,7 @@ $current_user_role =  trim($user_role);
     </label>
     </div>-->
     </li>
-	</div>
+	<!--</div>-->
 
 </script>
 
@@ -819,24 +857,32 @@ $current_user_role =  trim($user_role);
 
 <script type="text/template" id="blank-card">
 	<div class="row-fluid">
-    <li class="span3 thumbspan card find-min" id="91">
+    <li class="span3 thumbspan card find-min full-width" id="91">
     <div class="thumbnail select-button-91" id="thumbnail-91">
     <div class="m1">
     <div class="caption">
-    <!--<div class="minyawns-img">
+	<div class="row-fluid">
+	<div class="span3">
+    <div class="minyawns-img">
     <img src="<?php echo get_template_directory_uri(); ?>/images/avatar2.jpg" height="96" width="96"/>
-    </div>-->
+    </div>
+	</div>
+	<div class="span5 width40">
     <h4> Why wait for Minions to apply?</h4>
     <div class="text-meta-bold">With over 500 Minions equipped with valuable skills from top universities. 
     </div>
     <div class="text-meta">
     </div>
+	</div>
+	<div class="span4">
  <% if(result.job_owner_id === logged_in_user_id && result.user_to_job_status.indexOf('hired') == -1){ %>
-    <a href="<?php echo site_url() ?>/minyawns-directory" target="_blank" class="btn btn-primary">
+    <a href="<?php echo site_url() ?>/minyawns-directory" target="_blank" class="btn btn-primary m-t-40">
     <i class="icon-eye-open"></i>
     View All Minions
     </a>
     <% } %>
+	</div>
+	</div>
     </div>
     </div>
 
@@ -844,6 +890,11 @@ $current_user_role =  trim($user_role);
     </li>
 	</div>
 	
+	<div class="row-fluid">
+	<div class="span12">
+	<h3 class="custom-h3 pull-left m-t-10">Job Images</h3>
+	</div>
+	</div>
 	<div class="isotope">
   <div class="grid-sizer"></div>
   
