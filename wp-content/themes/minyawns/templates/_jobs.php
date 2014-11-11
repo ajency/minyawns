4514f90edc0296411554794c86dd938d41b23138
nevesseq <neves@ajency.in>
2014-11-11 17:42:47 +0530

    verified div added to jobs profile

--- wp-content/themes/minyawns/templates/_jobs.php
+++ wp-content/themes/minyawns/templates/_jobs.php
@@ -626,6 +626,10 @@ $current_user_role =  trim($user_role);
         <% } %>
       <a href='<?php echo site_url(); ?>/profile/<%= result.user_id %>' target="_blank"><%= result.user_image%></a>
     </div>
+	<% if(result.is_verified === 'Y'){%>
+    <!-- <img class="verfied" src="<?php echo get_template_directory_uri(); ?>/images/verifed.png" />-->
+    <div class="verfied-txt-job-prof">Verified Minion</div>
+    <% } %>
     <div class="rating full-width">
     <a href="#fakelink" id="thumbs_up_<%= result.user_id %>">
     <i class="icon-thumbs-up" ></i> <span class="thumbs_up_counts"><%= result.rating_positive %></span>
@@ -639,10 +643,7 @@ $current_user_role =  trim($user_role);
 	<div class="row-fluid">
 	<div class="span8">
     <h4 class="align-left"><a href='<?php echo site_url(); ?>/profile/<%= result.user_id %>' target="_blank"> <%= result.name %></a></h4>
-    <% if(result.is_verified === 'Y'){%>
-    <!-- <img class="verfied" src="<?php echo get_template_directory_uri(); ?>/images/verifed.png" />-->
-    <div class="verfied-txt">Verified Minion</div>
-    <% } %>
+    
     <div class="collage align-left"> <%= result.college%> </div>
     <div class="collage align-left"> <%= result.major%> </div>
     <div class="social-link align-left">
